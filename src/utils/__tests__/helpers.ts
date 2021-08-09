7851e1788c6302165c22982df4a6107894f9f804
Rubens Batista <batistarubinho@gmail.com>
2021-08-09 00:49:30 -0300

    refactor: organize tests

---
+++ src/utils/__tests__/helpers.ts
@@ -0,0 +1,12 @@
+import { formatDate } from 'utils/helpers';
+
+describe('Helpers', () => {
+  describe('formatDate', () => {
+    test('should format date correctly', () => {
+      // For some reason came like (yyyy-mm-dd)
+      // instead of (dd/mm/yyyy) as came in browser
+
+      expect(formatDate('2021-04-20T00:28:09.426Z')).toBe('2021-04-19');
+    });
+  });
+});
--- src/utils/__tests__/helpers/format-date.ts
+++
@@ -1,8 +0,0 @@
-import { formatDate } from 'utils/helpers';
-
-test('should format date correctly', () => {
-  // For some reason came like (yyyy-mm-dd)
-  // instead of (dd/mm/yyyy) as came in browser
-
-  expect(formatDate('2021-04-20T00:28:09.426Z')).toBe('2021-04-19');
-});
